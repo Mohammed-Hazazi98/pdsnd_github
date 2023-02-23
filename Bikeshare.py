b6e2cdfa61557fd83404075f0fb644678a1136d6
ZEX-Udacity26 <mhazazi0030@stu.kau.edu.sa>
2023-02-23 11:46:44 +0300

    refactor: Added a comment line in the code

--- Bikeshare.py
+++ Bikeshare.py
@@ -162,6 +162,7 @@ def trip_duration_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
+# The statistics on bikeshare users:
 def user_stats(df) :
         """Displays statistics on bikeshare users."""
 
